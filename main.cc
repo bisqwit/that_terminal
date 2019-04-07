8927905051a0a58ae4ec5cb1594697e73906467c
Joel Yliluoma <joel.yliluoma@iki.fi>
2019-04-07 21:06:43 +0300

    Add keyboard input support

--- main.cc
+++ main.cc
@@ -1,3 +1,5 @@
+#include <map>
+#include <unordered_map>
 #include <SDL.h>
 
 #include "terminal.hh"
@@ -8,7 +10,7 @@
 #include <poll.h>
 #include <unistd.h>
 
-unsigned VidCellWidth = 8, VidCellHeight = 8;
+unsigned VidCellWidth = 8, VidCellHeight = 16;
 unsigned WindowWidth  = 80, WindowHeight = 25;
 
 namespace
@@ -40,7 +42,7 @@ namespace
         {
             window = SDL_CreateWindow("terminal",
                 SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                pixels_width, pixels_height,
+                pixels_width*4, pixels_height*4,
                 SDL_WINDOW_RESIZABLE);
         }
         else
@@ -119,7 +121,9 @@ int main()
     std::string outbuffer;
 
     SDL_ReInitialize(wnd.xsize, wnd.ysize);
+    SDL_StartTextInput();
 
+    std::unordered_map<int, bool> keys;
     for(;;)
     {
         struct pollfd p[2] = { { tty.getfd(), POLLIN, 0 },
@@ -171,6 +175,79 @@ int main()
                     break;
                 case SDL_QUIT:
                     break;
+                case SDL_TEXTINPUT:
+                    tty.Send(ev.text.text);
+                    break;
+                case SDL_KEYDOWN:
+                case SDL_KEYUP:
+                {
+                    keys[ev.key.keysym.sym] = (ev.type == SDL_KEYDOWN);
+                    if(ev.type == SDL_KEYDOWN)
+                    {
+                        static const std::unordered_map<int, std::pair<int/*num*/,char/*letter*/>> lore
+                        {
+                            { SDLK_F1,       {1,'P'} },  { SDLK_LEFT,     {1,'D'} },
+                            { SDLK_F2,       {1,'Q'} },  { SDLK_RIGHT,    {1,'C'} },
+                            { SDLK_F3,       {1,'R'} },  { SDLK_UP,       {1,'A'} },
+                            { SDLK_F4,       {1,'S'} },  { SDLK_DOWN,     {1,'B'} },
+                            { SDLK_F5,       {15,'~'} }, { SDLK_HOME,     {1,'H'} },
+                            { SDLK_F6,       {17,'~'} }, { SDLK_END,      {1,'F'} },
+                            { SDLK_F7,       {18,'~'} }, { SDLK_INSERT,   {2,'~'} },
+                            { SDLK_F8,       {19,'~'} }, { SDLK_DELETE,   {3,'~'} },
+                            { SDLK_F9,       {20,'~'} }, { SDLK_PAGEUP,   {5,'~'} },
+                            { SDLK_F10,      {21,'~'} }, { SDLK_PAGEDOWN, {6,'~'} },
+                            { SDLK_F11,      {23,'~'} },
+                            { SDLK_F12,      {24,'~'} },
+                        };
+                        bool shift = keys[SDLK_LSHIFT] || keys[SDLK_RSHIFT];
+                        bool alt   = keys[SDLK_LALT]   || keys[SDLK_RALT];
+                        bool ctrl  = keys[SDLK_LCTRL]  || keys[SDLK_RCTRL];
+                        if(auto i = lore.find(ev.key.keysym.sym); i != lore.end())
+                        {
+                            const auto& d = i->second;
+                            unsigned delta = 1 + shift*1 + alt*2 + ctrl*4, len;
+                            char bracket = '[', Buf[16];
+                            if(d.second >= 'P' && d.second <= 'S') bracket = 'O';
+                            if(delta != 1)
+                                len = std::sprintf(Buf, "\33%c%d;%d%c", bracket, d.first, delta, d.second);
+                            else if(d.first == 1)
+                                len = std::sprintf(Buf, "\33%c%c", bracket, d.second);
+                            else
+                                len = std::sprintf(Buf, "\33%c%d%c", bracket, d.first, d.second);
+                            tty.Send(std::string_view(Buf, len));
+                        }
+                        else if(ctrl && (ev.key.keysym.sym >= SDLK_a && ev.key.keysym.sym <= SDLK_z))
+                        {
+                            char c = ev.key.keysym.sym - SDLK_a + 1;
+                            tty.Send(std::string_view(&c,1));
+                        }
+                        else if(ctrl && ev.key.keysym.sym == SDLK_2)
+                        {
+                            char c = 0;
+                            tty.Send(std::string_view(&c,1));
+                        }
+                        else if(ctrl && ev.key.keysym.sym >= SDLK_3 && ev.key.keysym.sym <= SDLK_9)
+                        {
+                            char c = ev.key.keysym.sym - SDLK_3 + 27;
+                            tty.Send(std::string_view(&c,1));
+                        }
+                        else switch(ev.key.keysym.sym)
+                        {
+                            case SDLK_RETURN:
+                                tty.Send("\r");
+                                break;
+                            case SDLK_BACKSPACE:
+                                if(ctrl) tty.Send("\10");
+                                else     tty.Send("\b");
+                                break;
+                            case SDLK_TAB:
+                                if(shift) tty.Send("\33[[Z");
+                                else      tty.Send("\t");
+                                break;
+                        }
+                    }
+                    break;
+                }
             }
 
         SDL_ReDraw(wnd);
