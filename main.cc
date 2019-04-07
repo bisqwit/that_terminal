890b7511fc15c81556bf0c18f00647e375d01fe6
Joel Yliluoma <joel.yliluoma@iki.fi>
2019-04-07 19:42:15 +0300

    Screen resizing support

--- main.cc
+++ main.cc
@@ -8,21 +8,37 @@
 #include <poll.h>
 #include <unistd.h>
 
+unsigned VidCellWidth = 8, VidCellHeight = 8;
+unsigned WindowWidth  = 80, WindowHeight = 25;
+
 namespace
 {
     SDL_Window*   window   = nullptr;
     SDL_Renderer* renderer = nullptr;
     SDL_Texture*  texture  = nullptr;
+    unsigned cells_horiz, cell_width_pixels,  pixels_width,  bufpixels_width, texturewidth;
+    unsigned cells_vert,  cell_height_pixels, pixels_height, bufpixels_height, textureheight;
     std::vector<std::uint32_t> pixbuf;
-    std::size_t texturewidth=0, textureheight=0;
 
-    void InitGFX(std::size_t pixels_width, std::size_t pixels_height)
+    void SDL_ReInitialize(unsigned cells_horizontal, unsigned cells_vertical)
     {
-        std::size_t bufpixels_width  = pixels_width;
-        std::size_t bufpixels_height = pixels_height;
+        cells_horiz = cells_horizontal;
+        cells_vert  = cells_vertical;
+        cell_width_pixels  = VidCellWidth;
+        cell_height_pixels = VidCellHeight;
+        pixels_width  = cells_horizontal * cell_width_pixels,
+        pixels_height = cells_vertical   * cell_height_pixels;
+        bufpixels_width = cells_horizontal * VidCellWidth;
+        bufpixels_height = cells_vertical  * VidCellHeight;
+        fprintf(stderr, "Cells: %ux%u, pix sizes: %ux%u (%u), pixels: %ux%u, buf: %ux%u\n",
+            cells_horiz,cells_vert,
+            cell_width_pixels,cell_height_pixels, VidCellHeight,
+            pixels_width,pixels_height,
+            bufpixels_width,bufpixels_height);
+
         if(!window)
         {
-            window = SDL_CreateWindow("editor",
+            window = SDL_CreateWindow("terminal",
                 SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
                 pixels_width, pixels_height,
                 SDL_WINDOW_RESIZABLE);
@@ -35,6 +51,7 @@ namespace
         {
             renderer = SDL_CreateRenderer(window, -1, 0);
         }
+
         if(texture && (texturewidth<bufpixels_width || textureheight<bufpixels_height))
         {
             SDL_DestroyTexture(texture);
@@ -50,16 +67,58 @@ namespace
 
         pixbuf.resize(bufpixels_width*bufpixels_height);
     }
+    void SDL_ReDraw(Window& wnd)
+    {
+        SDL_Rect rect;
+        rect.x=0; rect.w=bufpixels_width;
+        rect.y=0; rect.h=0;
+        unsigned errors = 0;
+        auto RenderFlushLines = [&]()
+        {
+            if(rect.h)
+            {
+                int w,h;
+                SDL_GetWindowSize(window, &w,&h);
+
+                SDL_Rect trect;
+                trect.x = rect.x * w / bufpixels_width;
+                trect.y = rect.y * h / bufpixels_height;
+                trect.w = (rect.x + rect.w) * w / bufpixels_width - trect.x;
+                trect.h = (rect.y + rect.h) * h / bufpixels_height - trect.y;
+
+                if(SDL_UpdateTexture(texture, &rect,
+                                     pixbuf.data() + rect.y*bufpixels_width,
+                                     bufpixels_width*sizeof(pixbuf[0]))) ++errors;
+                if(SDL_RenderCopy(renderer, texture, &rect, &trect)) ++errors;
+                //fprintf(stderr, "updated %u-%u\n", rect.y, rect.y+rect.h);
+                rect.y += rect.h;
+                rect.h = 0;
+            }
+        };
+        auto RenderAddLine = [&](unsigned line)
+        {
+            if(line > rect.y+rect.h+15 || line < rect.y) RenderFlushLines();
+            if(!rect.h) { rect.y = line; rect.h = 1; }
+            else rect.h = line+1-rect.y;
+        };
+
+        wnd.Render(VidCellWidth,VidCellHeight, &pixbuf[0]);
+        for(unsigned y=0; y<cells_vert*VidCellHeight; ++y)
+            RenderAddLine(y);
+
+        RenderFlushLines();
+        if(rect.y) { SDL_RenderPresent(renderer); }
+    }
 }
 
 int main()
 {
-    Window wnd(80, 25);
+    Window wnd(WindowWidth, WindowHeight);
     termwindow term(wnd);
     ForkPTY tty(wnd.xsize, wnd.ysize);
     std::string outbuffer;
 
-    InitGFX(80*8, 25*16);
+    SDL_ReInitialize(wnd.xsize, wnd.ysize);
 
     for(;;)
     {
@@ -95,12 +154,25 @@ int main()
             if(r > 0)
                 outbuffer.erase(0, r);
         }
-        wnd.Render(8,16, &pixbuf[0]);
 
-        SDL_UpdateTexture(texture, nullptr,
-                          pixbuf.data(),
-                          80*8*sizeof(pixbuf[0]));
-        SDL_RenderCopy(renderer, texture, nullptr, nullptr);
-        SDL_RenderPresent(renderer);
+        for(SDL_Event ev; SDL_PollEvent(&ev); )
+            switch(ev.type)
+            {
+                case SDL_WINDOWEVENT:
+                    switch(ev.window.event)
+                    {
+                        case SDL_WINDOWEVENT_EXPOSED:
+                        case SDL_WINDOWEVENT_RESIZED:
+                        case SDL_WINDOWEVENT_SIZE_CHANGED:
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+                case SDL_QUIT:
+                    break;
+            }
+
+        SDL_ReDraw(wnd);
     }
 }
