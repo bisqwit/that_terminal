d5b5898bcd030175d24051f52e6e5a2adf86ae69
Joel Yliluoma <joel.yliluoma@iki.fi>
2019-04-25 22:48:10 +0300

    No longer fail compilation of X11 is not available

--- beeper.cc
+++ beeper.cc
@@ -6,37 +6,45 @@
 #include <SDL.h>
 #include <SDL_syswm.h>
 
-/* TODO: Remove X11 dependency (put it behind #ifdefs) */
-
-#include <X11/Xlib.h>
+#ifdef SDL_VIDEO_DRIVER_X11
+# include <X11/Xlib.h>
+#endif
+#ifdef SDL_VIDEO_DRIVER_WINDOWS
+# include <Utilapiset.h>
+#endif
 
 extern SDL_Window* window;
 
 void BeepOn()
 {
-  #ifdef _WIN32
-    // TODO: Use some Microsoft thing
-  #endif
-
     SDL_SysWMinfo info = {};
     SDL_VERSION(&info.version);
     if(!SDL_GetWindowWMInfo(window,&info))
         fprintf(stderr, "GetWindowWMInfo failed %s\n", SDL_GetError());
 
-    auto display = info.info.x11.display;
-    if(display)
+#ifdef SDL_VIDEO_DRIVER_X11
+    if(info.subsystem == SDL_SYSWM_X11)
     {
-        XKeyboardControl ctrl = {};
-        ctrl.bell_percent = 100;
-        ctrl.bell_pitch   = 440;
-        ctrl.bell_duration = 50;
-        int ret1 = XChangeKeyboardControl(display, KBBellPitch|KBBellPercent|KBBellDuration, &ctrl);
-
-        int ret = XBell(display, 100);
-        std::fprintf(stderr, "belled? %d %d\n", ret1, ret);
+        if(auto display = info.info.x11.display)
+        {
+            XKeyboardControl ctrl = {};
+            ctrl.bell_percent = 100;
+            ctrl.bell_pitch   = 440;
+            ctrl.bell_duration = 50;
+            XChangeKeyboardControl(display, KBBellPitch|KBBellPercent|KBBellDuration, &ctrl);
+            XBell(display, 100);
+            return;
+        }
     }
-    else
+#endif
+#ifdef SDL_VIDEO_DRIVER_WINDOWS
+    if(info.subsystem == SDL_SYSWM_WINDOWS)
     {
-        std::fprintf(stderr, "display opening failed\n");
+        Beep(440, 50);
+        return;
     }
+#endif
+    // Unknown video driver.
+    // TODO: Create an audio interface.
+    // TODO: Synthesize beep.
 }
