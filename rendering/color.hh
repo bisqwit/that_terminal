32218cd456275af92d20e75bf9f120bdaea2fe1f
Joel Yliluoma <joel.yliluoma@iki.fi>
2021-10-26 20:46:58 +0300

    Documentation

--- rendering/color.hh
+++ rendering/color.hh
@@ -2,10 +2,12 @@
 #include <string>
 #include <algorithm>
 
+/* Unpack an RGB color in a 24-bit integer to its constituent r,g,b components */
 inline constexpr std::array<unsigned,3> Unpack(unsigned rgb)
 {
     return { rgb>>16, (rgb>>8)&0xFF, rgb&0xFF };
 }
+/* Pack an RGB color represented in three color channels into a single 24-bit integer */
 inline constexpr unsigned Repack(std::array<unsigned,3> rgb)
 {
     if(rgb[0] > 255 || rgb[1] > 255 || rgb[2] > 255) [[unlikely]]
@@ -27,6 +29,7 @@ inline constexpr unsigned Repack(std::array<unsigned,3> rgb)
          + (rgb[2] << 0);
 }
 
+/* Mix two colors such that the resulting color is (a*fac1 + b*fac2) / sum. */
 inline constexpr unsigned Mix(unsigned color1,unsigned color2, unsigned fac1,unsigned fac2,unsigned sum)
 {
     auto a = Unpack(color1), b = Unpack(color2);
@@ -34,13 +37,16 @@ inline constexpr unsigned Mix(unsigned color1,unsigned color2, unsigned fac1,uns
     return Repack(a);
 }
 
+/* Functions for parsing color names */
 unsigned ParseColorName(std::string_view s);
 unsigned ParseColorName(std::u32string_view s);
 
+/* Function for converting CMY into RGB */
 inline unsigned cmy2rgb(unsigned cmy)
 {
     return ~cmy & 0xFFFFFF;
 }
+/* Function for converting CMYK into RGB */
 inline unsigned cmyk2rgb(unsigned cmyk)
 {
     return Mix(0u, ~cmyk >> 8, cmyk & 255, ~cmyk & 255, 255);
