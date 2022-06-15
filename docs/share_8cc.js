728ba05372d429dc548766ca64b507eea017acf3
Joel Yliluoma <joel.yliluoma@iki.fi>
2022-06-15 12:17:27 +0300

    Update documentation after source changes

--- docs/share_8cc.js
+++ docs/share_8cc.js
@@ -1,8 +1,6 @@
 var share_8cc =
 [
-    [ "try_path", "share_8cc.html#a586b560b2b42947b81371a8306af97bf", null ],
-    [ "try_path", "share_8cc.html#a473c6ef05c9b9dd089497a01b4afaa2a", null ],
-    [ "FindCacheFile", "share_8cc.html#a29360ece01a12439270f558148c4090c", null ],
+    [ "FindCacheFile", "share_8cc.html#ae2ac35d6a748158efab4412954a59db2", null ],
     [ "FindShareFile", "share_8cc.html#a4a472ee81f6c1affad56809775c4dd00", null ],
     [ "SaveArg0", "share_8cc.html#a2dc78c734ec4b3626c60ace96e0f65fa", null ]
 ];
--- docs/share_8hh.js
+++ docs/share_8hh.js
@@ -1,6 +1,6 @@
 var share_8hh =
 [
-    [ "FindCacheFile", "share_8hh.html#ac3f76d8899a11a2df522b2e6725d9ab1", null ],
+    [ "FindCacheFile", "share_8hh.html#a885893070e15306b91483f32018dd91c", null ],
     [ "FindShareFile", "share_8hh.html#a39345f6c1bb313da223df493067db7eb", null ],
     [ "SaveArg0", "share_8hh.html#a5c5e0857d08430e7ac19e16ebd097c65", null ]
 ];
